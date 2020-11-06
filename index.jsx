dd384d6eaf45f57cdf18c3c047d17d98dc513ac7
Musa Barighzaai <musa@circleci.com>
2020-11-06 09:35:19 -0500

    fix yaml

--- index.jsx
+++ index.jsx
@@ -39,55 +39,60 @@ const Editor = () => {
         format: true,
         schemas: [
           {
-            $schema: "http://json-schema.org/draft-07/schema#",
-            title: "JSON Schema for Hadolint, a Dockerfile linter tool",
-            description:
-              "Dockerfile linter, validate inline bash, written in Haskell",
-            type: "object",
-            additionalProperties: false,
-            properties: {
-              ignored: {
-                type: "array",
-                description: "A list of rules to be ignored",
-                items: {
-                  type: "string",
-                  oneOf: [
-                    {
-                      const: "DL3000",
-                      description: "Use absolute WORKDIR.",
-                    },
-                    {
-                      const: "DL3001",
-                      description:
-                        "For some bash commands it makes no sense running them in a Docker container like ssh, vim, shutdown, service, ps, free, top, kill, mount, ifconfig.",
-                    },
-                    {
-                      const: "DL3002",
-                      description: "Last user should not be root.",
-                    },
-                    {
-                      const: "DL3003",
-                      description: "Use WORKDIR to switch to a directory.",
-                    },
-                    {
-                      const: "DL3004",
-                      description:
-                        "Do not use sudo as it leads to unpredictable behavior. Use a tool like gosu to enforce root.",
-                    },
-                    {
-                      const: "DL3005",
-                      description:
-                        "Do not use apt-get upgrade or dist-upgrade.",
-                    },
-                  ],
-                  title: "Rule",
+            uri: "http://myserver/foo-schema.json", // id of the first schema
+            fileMatch: ["*"], // associate with our model
+            schema: {
+              id: "http://myserver/foo-schema.json", // id of the first schema
+              $schema: "http://json-schema.org/draft-07/schema#",
+              title: "JSON Schema for Hadolint, a Dockerfile linter tool",
+              description:
+                "Dockerfile linter, validate inline bash, written in Haskell",
+              type: "object",
+              additionalProperties: false,
+              properties: {
+                ignored: {
+                  type: "array",
+                  description: "A list of rules to be ignored",
+                  items: {
+                    type: "string",
+                    oneOf: [
+                      {
+                        const: "DL3000",
+                        description: "Use absolute WORKDIR.",
+                      },
+                      {
+                        const: "DL3001",
+                        description:
+                          "For some bash commands it makes no sense running them in a Docker container like ssh, vim, shutdown, service, ps, free, top, kill, mount, ifconfig.",
+                      },
+                      {
+                        const: "DL3002",
+                        description: "Last user should not be root.",
+                      },
+                      {
+                        const: "DL3003",
+                        description: "Use WORKDIR to switch to a directory.",
+                      },
+                      {
+                        const: "DL3004",
+                        description:
+                          "Do not use sudo as it leads to unpredictable behavior. Use a tool like gosu to enforce root.",
+                      },
+                      {
+                        const: "DL3005",
+                        description:
+                          "Do not use apt-get upgrade or dist-upgrade.",
+                      },
+                    ],
+                    title: "Rule",
+                  },
                 },
-              },
-              trustedRegistries: {
-                type: "array",
-                description: "A list of trusted registries. Ex: docker.io",
-                items: {
-                  type: "string",
+                trustedRegistries: {
+                  type: "array",
+                  description: "A list of trusted registries. Ex: docker.io",
+                  items: {
+                    type: "string",
+                  },
                 },
               },
             },
